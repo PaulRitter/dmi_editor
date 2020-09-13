028de53747c0c172344fb769c04e122fe558f89e
Paul <ritter.paul1@googlemail.com>
2020-09-13 17:09:22 +0200

    fill stackoverflow fixed

--- DMIEditor/Tools/Fill.cs
+++ DMIEditor/Tools/Fill.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Text;
 using DMI_Parser.Extended;
 using DMIEditor.Undo;
@@ -20,22 +21,20 @@ namespace DMIEditor.Tools
                 return;
 
             FillPropagator fillPropagator = new FillPropagator(this);
-            fillPropagator.findPixels(oldColor, p);
+            List<Point> points = fillPropagator.findPixels(oldColor, p);
             List<PixelChangeItem> changeItems = new List<PixelChangeItem>();
-            foreach (var point in fillPropagator.Points)
+            foreach (var point in points)
             {
                 changeItems.Add(new PixelChangeItem(point, getPixel(point)));
             }
             main.UndoManager.RegisterUndoItem(new PixelChangeUndoItem(Layer, changeItems));
-            Layer.SetPixels(fillPropagator.Points.ToArray(), main.GetColor());
+            Layer.SetPixels(points.ToArray(), main.GetColor());
         }
 
         
 
         private class FillPropagator
         {
-            public readonly List<Point> Points = new List<Point>();
-            private List<Point> _alreadyProcessed = new List<Point>();
             private Size _bounds;
             private Bitmap _bitmap; //much better to just clone bitmap an make our requests here
 
@@ -45,31 +44,37 @@ namespace DMIEditor.Tools
                 _bitmap = fillTool.Layer.GetBitmap();
             }
 
-            //todo fix stackoverflow when pixels > 3637
-            //maybe this can be fixed by making this a loop?
-            public void findPixels(Color oldColor, Point pixel)
+            public List<Point> findPixels(Color oldColor, Point pixel)
             {
-                foreach (Point point in _alreadyProcessed)
+                List<Point> resultList = new List<Point>();
+
+                List<Point> alreadyProcessed = new List<Point>();
+                List<Point> workList = new List<Point> {pixel};
+                while (workList.Count > 0)
                 {
-                    if (point.Equals(pixel))
-                    {
-                        return;
-                    }
-                }
-                _alreadyProcessed.Add(pixel);
-                if (pixel.X >= _bounds.Width || pixel.X < 0)
-                    return;
-                if (pixel.Y >= _bounds.Height || pixel.Y < 0)
-                    return;
+                    Point p = workList[^1];
+                    workList.Remove(p);
+                    
+                    if (alreadyProcessed.Any(point => point.Equals(p)))
+                        continue;
+                    
+                    alreadyProcessed.Add(p);
+                    if (p.X >= _bounds.Width || p.X < 0)
+                        continue;
+                    if (p.Y >= _bounds.Height || p.Y < 0)
+                        continue;
 
-                if (!oldColor.Equals(_bitmap.GetPixel(pixel.X, pixel.Y))) return;
+                    if (!oldColor.Equals(_bitmap.GetPixel(p.X, p.Y))) continue;
             
-                Points.Add(pixel);
+                    resultList.Add(p);
+                    
+                    workList.Add(new Point(p.X + 1, p.Y));
+                    workList.Add(new Point(p.X - 1, p.Y));
+                    workList.Add(new Point(p.X, p.Y + 1));
+                    workList.Add(new Point(p.X, p.Y - 1));
+                }
 
-                findPixels(oldColor, new Point(pixel.X + 1, pixel.Y));
-                findPixels(oldColor, new Point(pixel.X - 1, pixel.Y));
-                findPixels(oldColor, new Point(pixel.X, pixel.Y + 1));
-                findPixels(oldColor, new Point(pixel.X, pixel.Y - 1));
+                return resultList;
             }
         }
     }
